851104701f28220e1d52eceed9451403f29c3471
the-codeboy <71213855+the-codeboy@users.noreply.github.com>
2021-10-10 19:41:35 +0200

    Add Config and ConfigScope annotation

---
+++ src/main/java/ml/codeboy/bukkitbootstrap/config/Config.java
@@ -0,0 +1,28 @@
+package ml.codeboy.bukkitbootstrap.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Indicates that this class is being serialized by {@link ConfigReader}
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Config {
+    /**
+     * @return the name of the file this class should be serialized as by default
+     */
+    String name() default "";
+
+    /**
+     * @return the comments to be added to the start of the yml file
+     */
+    String comments() default "";
+
+    /**
+     * @return which fields should be part of the config
+     */
+    ConfigScope scope() default ConfigScope.ALL;
+}
---
+++ src/main/java/ml/codeboy/bukkitbootstrap/config/ConfigScope.java
@@ -0,0 +1,5 @@
+package ml.codeboy.bukkitbootstrap.config;
+
+public enum ConfigScope {
+    NONE,ALL,PUBLIC,PRIVATE;
+}
