b04a26167e6ec6ebe16548947207ab2f0ca6afa9
the-codeboy <71213855+the-codeboy@users.noreply.github.com>
2022-01-03 01:15:01 +0100

    Add utility class

---
+++ src/main/java/ml/codeboy/bukkitbootstrap/util/Util.java
@@ -0,0 +1,24 @@
+package ml.codeboy.bukkitbootstrap.util;
+
+public class Util {
+    public static String replaceAllCodes(String toTranslate){
+        return translateHexColorCodes("\\{","\\}", ChatColor.translateAlternateColorCodes('&',toTranslate));
+    }
+
+    public static String translateHexColorCodes(String startTag, String endTag, String message)
+    {
+        final Pattern hexPattern = Pattern.compile(startTag + "([A-Fa-f0-9]{6})" + endTag);
+        Matcher matcher = hexPattern.matcher(message);
+        StringBuffer buffer = new StringBuffer(message.length() + 4 * 8);
+        while (matcher.find())
+        {
+            String group = matcher.group(1);
+            matcher.appendReplacement(buffer, COLOR_CHAR + "x"
+                    + COLOR_CHAR + group.charAt(0) + COLOR_CHAR + group.charAt(1)
+                    + COLOR_CHAR + group.charAt(2) + COLOR_CHAR + group.charAt(3)
+                    + COLOR_CHAR + group.charAt(4) + COLOR_CHAR + group.charAt(5)
+            );
+        }
+        return matcher.appendTail(buffer).toString();
+    }
+}
